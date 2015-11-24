517e4698883572e3d2abcf72b38d7cb759b1e3e1
jaudas <jessica.audas@epfedu.fr>
2015-11-24 11:12:53 +0100

    loin et signup final

--- app/scripts/appModule.js
+++ app/scripts/appModule.js
@@ -2,7 +2,7 @@ var OTR = angular.module('OTR', ['ngRoute']);
 
 OTR.config(function ($routeProvider) {
     //Cette config permet de  diriger le ng-view entre les pages
-    //Permet également de déclarer le controlleur associé
+    //Permet également de déclarer le controller associé
     $routeProvider
       .when('/', {
         templateUrl: 'app/views/main.html', 
@@ -15,6 +15,14 @@ OTR.config(function ($routeProvider) {
       .when('/login', {
         templateUrl: 'app/views/login.html',
         controller: 'LoginCtrl'
+      })
+      .when('/terms-of-service',{
+        templateUrl : 'app/views/terms-of-service.html',
+        controller : ''
+      })
+      .when('/blog',{
+        templateUrl : 'app/views/blog.html',
+        controller : ''
       })
       .otherwise({ //page d'erreur sympatique
         templateUrl: 'app/views/404.html',
