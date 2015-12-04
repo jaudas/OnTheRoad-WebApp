638dd2045a3dac2eea341df719f184c4d1379508
jaudas <jessica.audas@epfedu.fr>
2015-12-04 19:27:24 +0100

    manageoffers V1

--- app/scripts/controllers/ManageOffersCtrl.js
+++ app/scripts/controllers/ManageOffersCtrl.js
@@ -1,6 +1,34 @@
-OTR.controller("ManageOffersCtrl", function($scole, $http){
+OTR.controller("ManageOffersCtrl", function($scope, $http){
+
+	var SERVEUR_URL = "";
+	var MOCK_URL = "mockup.json" 
+
+	$scope.offers = [];
+	$scope.isLoading = false;
 
 
+	//Fonction permettant de récupérer les offres dans la base de données
+	function getOffers()
+	{
+		console.info("getting offers");
+		$scope.isLoading =true;
+		$http.get(MOCK_URL)
+		.success(function(data,status)
+		{
+			$scope.offers=data;
+			$scope.status=status;
+			$scope.isLoading = false;
+		})
+		.error(function(data,status)
+		{
+			$scope.offers=data;
+			console.error('unable to load offers'+ status);
+			$scope.isLoading = false;
+		})
+	}
+
+
+	getOffers();
 
 
 });
