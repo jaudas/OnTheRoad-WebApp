77490bddb4a62538c903608c1b977ab9174be077
jaudas <jessica.audas@epfedu.fr>
2015-12-14 00:06:24 +0100

    final commit

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -1,12 +1,21 @@
 OTR.controller("LoginCtrl", function($scope, $http){
 
-$http.post('localhost:8080//login',{'mail' : $scope.mail,'password' : $scope.password})
+	var SERVEUR_URL = "http://193.50.54.5:8080/API_OnTheRoad_Spring-0.0.1-SNAPSHOT/login";
+
+$http.post(SERVEUR_URL,{'mail' : $scope.mail,'password' : $scope.password})
            .success(function(data) {
-                $scope.feedBack=data;   
+                $scope.offers=data;
+                $scope.status=status;
+                $scope.isLoading = false; 
+
           })
           .error(function(data) {
            $scope.feedBack= "Une erreur est survenue";
           });
 
+
+
+
+	
 
 });
